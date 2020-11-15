8cd04331618f9ace853b09361b103987e48c5810
SardarDawar <dawarsardar786@gmail.com>
2020-11-16 00:49:16 +0500

    new apis

--- bfinder/settings.py
+++ bfinder/settings.py
@@ -107,24 +107,24 @@ WSGI_APPLICATION = 'bfinder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'defaultdb',
-#        'USER': 'doadmin',
-#        'PASSWORD':'u02pume6b1taqfoj',
-#        'HOST':'clouddatabase-do-user-7851294-0.a.db.ondigitalocean.com',
-#        'PORT':'25060'
-
-#  }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 # }
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.mysql',
+       'NAME': 'defaultdb',
+       'USER': 'doadmin',
+       'PASSWORD':'u02pume6b1taqfoj',
+       'HOST':'clouddatabase-do-user-7851294-0.a.db.ondigitalocean.com',
+       'PORT':'25060'
+
+ }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
